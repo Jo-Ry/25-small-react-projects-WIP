cd3e1fe5461240986a81f43e26c0ea0057d2d6ba
Johannes <johannes.ryberg@gmail.com>
2025-03-16 22:06:19 +0100

    ComponentWrapper now accepts a new prop and added som default styling for the other view aswell

--- src/components/ComponentWrapper.tsx
+++ src/components/ComponentWrapper.tsx
@@ -3,15 +3,16 @@ import { CSSProperties, ReactNode } from 'react';
 type ComponentWrapperType = {
     view: 'viewport' | 'fill';
     title?: string
-    style?: CSSProperties
+    style?: CSSProperties,
+    className?: string,
     children: ReactNode;
 };
 
-const ComponentWrapper = ({ view, title, style, children }: ComponentWrapperType) => {
+const ComponentWrapper = ({ view, title, style, className, children }: ComponentWrapperType) => {
     return (
         <section style={style}>
             <h2 className='title'>{title}</h2>
-            <div className= {`base ${view === 'fill' ? '' : 'viewport'}`}>
+            <div className= {`base ${view === 'fill' ? 'fill' : 'viewport'} ${className ? className : ''}`}>
             {children}
             </div>
         </section>
