35384127a5e3fb88b7277fcf93ab1cdeebd2acff
Johannes <johannes.ryberg@gmail.com>
2025-03-26 20:55:22 +0100

    added a prop to <ComponentWrapper/>

--- src/components/ComponentWrapper.tsx
+++ src/components/ComponentWrapper.tsx
@@ -3,15 +3,16 @@ import { CSSProperties, ReactNode } from 'react';
 type ComponentWrapperType = {
     view: 'viewport' | 'fill' | '';
     title?: string
+    titleClassname?: string
     style?: CSSProperties,
     className?: string,
     children: ReactNode;
 };
 
-const ComponentWrapper = ({ view, title, style, className, children }: ComponentWrapperType) => {
+const ComponentWrapper = ({ view, title, titleClassname, style, className, children }: ComponentWrapperType) => {
     return (
         <section style={style}>
-            {title && <h2 className='title'>{title}</h2>}
+            {title && <h2 className={`title ${titleClassname}`}> {title}</h2>}
             <div className= {`${view === '' ? '' : 'base '}${view}${className ? ' ' + className : ''}`}>
             {children}
             </div>
