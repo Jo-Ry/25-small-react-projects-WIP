5d15cc827e949948634dec8e6d9735d4ba4cef84
Johannes <johannes.ryberg@gmail.com>
2025-04-02 00:53:03 +0200

    project-16 = create a custom hoook that detects when user presses outside the respective element, this is now used with the <Modal> component from project-11

---
+++ src/hooks/useOnClickOutsideComponent.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+
+/**
+ * A custom React hook that triggers a handler function when a click occurs outside of the specified element.
+ *
+ * @template T - A React ref object pointing to the target element.
+ * @param ref - A React ref object that references the element to detect outside clicks for.
+ * @param handler - A callback function to be executed when a click outside the referenced element is detected.
+ *
+ * @remarks
+ * - The `handler` function is called with the `MouseEvent` as its argument.
+ * - The hook uses the `mousedown` event to detect clicks outside the element.
+ */
+const UseOnClickOutsideComponent = <T extends React.RefObject<unknown>>(
+    ref: T,
+    handler: (event: Event) => void,
+) => {
+    useEffect(() => {
+        
+        /**
+         * Handles click events outside of a specified element.
+         *
+         * @param event - The mouse event triggered by a click.
+         *
+         * The function checks if the clicked target is outside the element referenced by `ref`.
+         * If the target is outside, it invokes the provided `handler` function.
+         * If the target is inside the element or the element does not exist, the function returns early.
+         */
+        const handleClickOutside = (event: MouseEvent) => {
+            const element = (ref as T).current as HTMLElement;
+            if (!element || element.contains(event.target as Node)) {
+                console.log('in');
+                return;
+            }
+            console.log('out');
+
+            handler(event);
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [handler, ref]);
+};
+
+export default UseOnClickOutsideComponent;
--- src/project-11/Modal.tsx
+++ src/project-11/Modal.tsx
@@ -1,7 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import ComponentWrapper from '../components/ComponentWrapper';
+import useOnClickOutsideComponent from '../hooks/useOnClickOutsideComponent';
 
 const Modal = () => {
+    return (
+        <ComponentWrapper view="viewport" className='modal' title="open and close modal">
+            <ModalUsingDialog />
+            <ModalUsingDiv />
+        </ComponentWrapper>
+    );
+};
+
+export default Modal;
+
+/**
+ * This component demonstrates that the <dialog> element is a valid
+ * and accessible way to implement a modal in React.
+ */
+const ModalUsingDialog = () => {
     const modalRef = useRef<HTMLDialogElement>(null);
 
     const openModal = () => {
@@ -12,16 +28,54 @@ const Modal = () => {
         }
     };
 
+    const closeModal = () => {
+        modalRef.current?.close();
+    };
+
     return (
-        <ComponentWrapper view="viewport" title="open and close modal">
+        <>
             <button onClick={() => openModal()}>open dialog</button>
-            <dialog className='modal' ref={modalRef}>
+
+            <dialog ref={modalRef}>
                 <h2>Modal</h2>
                 <p>Modal content</p>
-                <button onClick={() => modalRef.current?.close()}>close dialog</button>
+                <button onClick={() => closeModal()}>close dialog</button>
             </dialog>
-        </ComponentWrapper>
+        </>
     );
 };
 
-export default Modal;
+/**
+ * This component's primary purpose is not to display a visually appealing modal,
+ * but to demonstrate the functionality of the useOnClickOutsideComponent hook.
+ */
+const ModalUsingDiv = () => {
+    const [showContent, setShowContent] = useState(false);
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    // use the custom hook
+    useOnClickOutsideComponent(modalRef, () => setShowContent(false));
+
+    const openModal = () => {
+        setShowContent(true);
+    };
+
+    const closeModal = () => {
+        setShowContent(false);
+    };
+
+    return (
+        <>
+            <div ref={modalRef}>
+                <button onClick={() => openModal()}>open div</button>
+                <div className={`${showContent ? '' : ' hide'}`}>
+                    <div className='modal__container'>
+                        <h2>Modal</h2>
+                        <p>Press outside of this modal</p>
+                        <button onClick={() => closeModal()}>close div</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
